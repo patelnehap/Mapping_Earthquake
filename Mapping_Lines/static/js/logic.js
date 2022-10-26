59c9a861aae115c56062353b678f0653401d425d
patelnehap <patelnehap@yahoo.com>
2022-10-26 11:30:30 -0400

    13.4.3 Updates

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -1,17 +1,20 @@
 // Add console.log to check to see if our code is working.
 console.log("working");
 
-// Create the map object with a center and zoom level.
-let map = L.map('mapid').setView([40.7, -94.5], 2);
+// Create the map object with center at the San Francisco airport.
+let map = L.map('mapid').setView([37.6213, -122.3790], 5);
 
+// Coordinates for each point to be used in the polyline.
 let line = [
   [33.9416, -118.4085],
-  [37.6213, -122.3790]
+  [37.6213, -122.3790],
+  [40.7899, -111.9791],
+  [47.4502, -122.3088]
 ];
 
 // Create a polyline using the line coordinates and make the line red.
 L.polyline(line, {
-  color: "red"
+  color: "yellow"
 }).addTo(map);
 
 // Get data from cities.js
